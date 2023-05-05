607930b1bf4920aa06df4eb4480ed0f105574761
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-05 19:49:59 -0300

    comentários req 01

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -15,13 +15,15 @@ abstract class Race {
 
   // retorna qtd máxima de pts de vida da raça
   // cada raça terá seu número máximo de pontos, que será definido dentro de cada classe especializada;
-  // nesta classe deve estar apenas a assinatura do método.
+  // nesta classe deve estar apenas a assinatura do método (por isso não tem return).
   abstract get maxLifePoints(): number;
 
+  // Apenas as classes herdeiras poderão dar nomes
   get name(): string {
     return this._name;
   }
 
+  // Apenas as classes herdeiras poderão dar destrezas
   get dexterity(): number {
     return this._dexterity;
   }
