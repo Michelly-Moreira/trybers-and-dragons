925507568732679511a01bc5aa2374f554479f41
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-08 18:39:03 -0300

    implementando os métodos na classe herdeira Mage

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -2,10 +2,20 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
-private static _createdArchetypeInstances = 0;
-private energyType: EnergyType;
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
 
-constructor() {
-  
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage._createdArchetypeInstances += 1;
 }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Mage._createdArchetypeInstances;
+  }
 }
