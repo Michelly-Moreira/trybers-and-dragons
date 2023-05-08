8f4fa1c98daba963048dd2bc4251edaad3836a8d
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-08 20:41:33 -0300

    atributos da classe Character inicializado no construtor

--- src/Character.ts
+++ src/Character.ts
@@ -1,5 +1,31 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Race, { Elf } from './Races';
 import Fighter from './Fighter';
 
 export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
 
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    this._defense = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: Math.floor(Math.random() * (10 - 1 + 1) + 1),
+    };
+  }
 }
