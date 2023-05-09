c9f97842d4708140cac935526919eaaf9a88d900
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-09 19:01:06 -0300

    req 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,29 +1,28 @@
 import Battle from './Battle';
 import Character from '../Character';
+import { SimpleFighter } from '../Fighter';
 
 export default class PVP extends Battle {
-  private _player1: Character;
-  private _player2: Character;
+  private _player1: Character | SimpleFighter;
+  private _player2: Character | SimpleFighter;
 
-  constructor(p1: Character, p2: Character) {
+  constructor(p1: Character | SimpleFighter, p2: Character | SimpleFighter) {
     super(p1);
     this._player1 = p1;
     this._player2 = p2;
   }
 
+  playersAttackEachOther(): void {
+    this._player2.attack(this._player1);
+    if (this._player1.lifePoints !== -1) {
+      this._player1.attack(this._player2);
+    }
+  }
+
   fight(): number {
     while (this._player1.lifePoints !== -1 && this._player2.lifePoints !== -1) {
-      this._player1.attack(this._player2);
-      if (this._player2.lifePoints === -1) {
-        return -1; // jogador 1 ganha
-      }
-
-      this._player2.attack(this._player1);
-      if (this._player1.lifePoints === -1) {
-        return 1; // jogador 2 ganha
-      }
+      this.playersAttackEachOther();
     }
-
-    return super.fight(); // In case of a draw
+    return super.fight();
   }
 }
