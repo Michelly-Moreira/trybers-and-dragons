d1c57a0afb08d23d13f1f703287a43ac5d378660
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-09 20:30:36 -0300

    criando objetos do  tipo Monster

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import Character from './Character';
+import Monster from './Monster';
+import Dragon from './Dragon';
 
 const player1 = new Character('Hanna');
 const player2 = new Character('Intruder');
 const player3 = new Character('Flyer');
 player1.levelUp();
 
-export { player1, player2, player3 };
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+export { player1, player2, player3, monster1, monster2 };
